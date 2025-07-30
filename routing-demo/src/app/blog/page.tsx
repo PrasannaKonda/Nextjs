7708b7bc36253e83cc188d759accb2dc369dc913
PrasannaKonda <kondalakshmiprasanna1804@gmail.com>
2025-07-30 15:17:57 +0530

    title metadata

--- routing-demo/src/app/blog/page.tsx
+++ routing-demo/src/app/blog/page.tsx
@@ -1,3 +1,17 @@
+import {Metadata} from "next"
+
+//template title
+// export const metadata:Metadata = {
+//     title:"blog"
+// }
+
+//Absolute title
+export const metadata:Metadata = {
+    title:{
+        absolute:"blog"
+    }
+}
+
 export default function Blog() {
     return <h1>My blog</h1>
 }
